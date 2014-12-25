8f12082a70e2b199837c9acb5c88194afef069fa
moldcraft <moldcraft@email.com>
2014-12-25 16:28:58 +0200

    added description

--- manifest.php
+++ manifest.php
@@ -3,7 +3,7 @@
 $manifest = array();
 
 $manifest['name'] = __('Mailer', 'fw');
-$manifest['description'] = __('Send emails...', 'fw');
+$manifest['description'] = __('This extension will let you set some global email options and it is used by other extensions (like Forms) to send emails.', 'fw');
 $manifest['version'] = '1.0.0';
 $manifest['standalone'] = false;
 $manifest['display'] = true;
