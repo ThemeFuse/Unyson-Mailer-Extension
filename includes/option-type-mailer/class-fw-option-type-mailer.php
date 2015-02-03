cade7c30276087dc27ac2ac3db97c28273e397d1
Pisarenco Gheorghe <pisarenco.gh@gmail.com>
2015-02-03 17:27:52 +0200

    Code reafactoring, Custom text review

--- includes/option-type-mailer/class-fw-option-type-mailer.php
+++ includes/option-type-mailer/class-fw-option-type-mailer.php
@@ -26,13 +26,14 @@ class FW_Option_Type_Mailer extends FW_Option_Type {
 	 */
 	protected function _get_defaults() {
 		return array(
-			'label' => false,
-			'type' => 'multi',
+			'label'         => false,
+			'type'          => 'multi',
 			'inner-options' => array(
-				'method' => array(
-					'label'   => __('Send Method', 'fw'),
-					'type'    => 'select',
-					'attr' => array(
+				'method'  => array(
+					'label'   => __( 'Send Method', 'fw' ),
+					'desc'    => __( 'Select the send form method', 'fw' ),
+					'type'    => 'short-select',
+					'attr'    => array(
 						'data-select-method' => 'select-method'
 					),
 					'value'   => 'wpmail',
@@ -41,78 +42,83 @@ class FW_Option_Type_Mailer extends FW_Option_Type {
 						'smtp'   => 'SMTP',
 					)
 				),
-				'smtp' => array(
-					'type'      => 'group',
-					'options'   => array(
+				'smtp'    => array(
+					'type'    => 'group',
+					'options' => array(
 						'smtp' => array(
 							'label'         => false,
 							'desc'          => false,
 							'type'          => 'multi',
-							'attr' => array(
+							'attr'          => array(
 								'data-method' => 'smtp'
 							),
 							'inner-options' => array(
-								'host' => array(
-									'label'     => __('Server Address', 'fw'),
-									'type'      => 'text',
-									'value'     => '',
+								'host'     => array(
+									'label' => __( 'Server Address', 'fw' ),
+									'desc'  => __( 'Enter your email server', 'fw' ),
+									'type'  => 'text',
+									'value' => '',
 								),
 								'username' => array(
-									'label'     => __('Username', 'fw'),
-									'type'      => 'text',
-									'value'     => '',
+									'label' => __( 'Username', 'fw' ),
+									'desc'  => __( 'Enter your username', 'fw' ),
+									'type'  => 'text',
+									'value' => '',
 								),
 								'password' => array(
-									'label'     => __('Password', 'fw'),
-									'type'      => 'password',
-									'value'     => '',
+									'label' => __( 'Password', 'fw' ),
+									'desc'  => __( 'Enter your password', 'fw' ),
+									'type'  => 'password',
+									'value' => '',
 								),
-								'secure' => array(
-									'label'     => __('Secure Connection', 'fw'),
-									'type'      => 'radio',
-									'value'     => 'no',
-									'choices'   => array(
+								'secure'   => array(
+									'label'   => __( 'Secure Connection', 'fw' ),
+									'type'    => 'radio',
+									'inline'  => true,
+									'value'   => 'no',
+									'choices' => array(
 										'no'  => 'No',
 										'ssl' => 'SSL',
 										'tls' => 'TLS'
 									)
 								),
-								'port' => array(
-									'label'     => __('Custom Port', 'fw'),
-									'desc'      => __('Optional - SMTP port number to use. Leave blank for default (SMTP - 25, SMTPS - 465).', 'fw'),
-									'type'      => 'text',
-									'attr'      => array(
+								'port'     => array(
+									'label' => __( 'Custom Port', 'fw' ),
+									'desc'  => __( 'Optional - SMTP port number to use.', 'fw' ),
+									'help'  => __( 'Leave blank for default (SMTP - 25, SMTPS - 465)', 'fw' ),
+									'type'  => 'text',
+									'attr'  => array(
 										'maxlength' => 5,
 									),
-									'value'     => '',
+									'value' => '',
 								),
 							),
-							'value' => array()
+							'value'         => array()
 						),
 					)
 				),
-				'general'   => array(
-					'label' => false,
-					'desc'  => false,
-					'type'  => 'multi',
+				'general' => array(
+					'label'         => false,
+					'desc'          => false,
+					'type'          => 'multi',
 					'inner-options' => array(
-						'from_name' => array(
-							'label'     => __('From Name', 'fw'),
-							'desc'      => __('The form will look like was sent from this name.', 'fw'),
-							'type'      => 'text',
-							'value'     => '',
+						'from_name'    => array(
+							'label' => __( 'From Name', 'fw' ),
+							'desc'  => __( "The name you'll see in the From filed in your email client.", 'fw' ),
+							'type'  => 'text',
+							'value' => '',
 						),
 						'from_address' => array(
-							'label'     => __('From Address', 'fw'),
-							'desc'      => __('The form will look like was sent from this address.', 'fw'),
-							'type'      => 'text',
-							'value'     => '',
+							'label' => __( 'From Address', 'fw' ),
+							'desc'  => __( 'The form will look like was sent from this email address.', 'fw' ),
+							'type'  => 'text',
+							'value' => '',
 						)
 					),
-					'value' => array()
+					'value'         => array()
 				)
 			),
-			'value' => array()
+			'value'         => array()
 		);
 	}
 
@@ -123,13 +129,13 @@ class FW_Option_Type_Mailer extends FW_Option_Type {
 	protected function _enqueue_static( $id, $option, $data ) {
 		wp_enqueue_style(
 			$this->get_type() . '-scripts',
-			fw_ext('mailer')->get_declared_URI() . '/includes/option-type-mailer/static/css/style.css'
+			fw_ext( 'mailer' )->get_declared_URI() . '/includes/option-type-mailer/static/css/style.css'
 		);
 
 		wp_enqueue_script(
 			$this->get_type() . '-scripts',
-			fw_ext('mailer')->get_declared_URI() . '/includes/option-type-mailer/static/js/scripts.js',
-			array( 'fw-events'),
+			fw_ext( 'mailer' )->get_declared_URI() . '/includes/option-type-mailer/static/js/scripts.js',
+			array( 'fw-events' ),
 			fw()->manifest->get_version(), true
 		);
 	}
@@ -140,7 +146,7 @@ class FW_Option_Type_Mailer extends FW_Option_Type {
 	protected function _render( $id, $option, $data ) {
 		$data['value'] = fw_ext( 'mailer' )->get_db_settings_option();
 
-		return fw()->backend->option_type('multi')->render( $id, $option, $data );
+		return fw()->backend->option_type( 'multi' )->render( $id, $option, $data );
 	}
 
 	/**
@@ -154,10 +160,10 @@ class FW_Option_Type_Mailer extends FW_Option_Type {
 	protected function _get_value_from_input( $option, $input_value ) {
 
 		if ( is_array( $input_value ) && ! empty( $input_value ) ) {
-			fw_ext('mailer')->set_db_settings_option( null, $input_value );
+			fw_ext( 'mailer' )->set_db_settings_option( null, $input_value );
 		}
 
-		return fw_ext('mailer')->get_db_settings_option();
+		return fw_ext( 'mailer' )->get_db_settings_option();
 	}
 }
 
